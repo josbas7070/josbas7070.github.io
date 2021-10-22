dff8d09fcdf0debc8f50293e4c4e560b258c63c2
josbas7070 <92938651+josbas7070@users.noreply.github.com>
2021-10-22 20:15:13 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,69 @@
+/*const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world hello !';
+let myVariable;
+myVariable='333';
+myVariable;
+myHeading.textContent= myVariable;
+let iceCream = 'chocolate';
+if(iceCream === 'chocolate') {
+  alert('Yay, I love chocolate ice cream!');
+} else {
+  alert('Awwww, but chocolate is my favorite...');
+}
+function multiply(num1,num2) {
+    let result = num1 * num2;
+    return result;
+  }
+  multiply(4,2);
+  alert('resultmultiply:'+ String( multiply(4.3)));*/
+  
+  /* Events -- on-click */
+ /* document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}*/
+/* these are equivalent */
+/*let myHTML = document.querySelector('html');
+myHTML.onclick = function() {
+    alert('Ouch! Stop poking me - second way !');
+};*/
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/COUCOU.JPG') {
+      myImage.setAttribute('src','images/CHRISTO.JPG');
+    } else {
+      myImage.setAttribute('src','images/COUCOU.JPG');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+}/*if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}*/
+myButton.onclick = function() {
+  setUserName();
+}
+
+
+
+
+
+
+
+
+
+
+
+
